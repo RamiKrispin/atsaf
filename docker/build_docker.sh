6e65881262517f1ce7be19fff4a99259beff77b7
RamiKrispin <ramkrisp@umich.edu>
2022-09-15 09:32:39 -0700

    add build arguments

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,7 +2,11 @@
 
 echo "Build the docker"
 
-docker build . --progress=plain --build-arg CONDA_ENV=atsaf -t rkrispin/atsaf:dev.0.0.0.9000
+docker build . --progress=plain \
+               --build-arg CONDA_ENV=atsaf \
+               --build-arg QUARTO_VERSION=1.1.149 \
+               --build-arg PYTHON_VER=3.10 \
+               -t rkrispin/atsaf:dev.0.0.0.9000
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
