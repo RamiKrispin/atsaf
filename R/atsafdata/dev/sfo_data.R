b5c93557d7d10cce1c517ae772d03c3f120c6876
RamiKrispin <ramkrisp@umich.edu>
2023-03-06 03:44:43 -0800

    move files to new folder

--- R/atsafdata/dev/sfo_data.R
+++ R/atsafdata/dev/sfo_data.R
@@ -6,7 +6,7 @@ df <- readr::read_csv("https://data.sfgov.org/resource/rkru-6vcg.csv") %>%
 head(df)
 
 `%>%` <- magrittr::`%>%`
-cmd <- "curl 'https://data.sfgov.org/resource/rkru-6vcg.json?$limit=25000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
+cmd <- "curl 'https://data.sfgov.org/resource/rkru-6vcg.json?$limit=50000' | jq -r '.[] |[.activity_period, .operating_airline, .operating_airline_iata_code,
 .published_airline, .published_airline_iata_code, .geo_summary, .geo_region,
 .activity_type_code, .price_category_code, .terminal, .boarding_area, .passenger_count] |  @tsv'"
 
